a9bb3f85fa1306adb345dd1d8475aa417fc315de
nate geslin <teamtomkins23@gmail.com>
2022-05-15 21:11:43 -0500

    adds ConfigModule and env var validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_DIALECT: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        POSTGRES_DB_DEVELOPMENT: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
